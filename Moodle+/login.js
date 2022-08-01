a450595a38db75ac4d49907b6282f865116b843b
priyallj05 <109871116+priyallj05@users.noreply.github.com>
2022-08-01 19:19:14 +0530

    Update login.js

--- Moodle+/login.js
+++ Moodle+/login.js
@@ -1,10 +1,41 @@
 console.log("Moodle+ successfully loaded!");
-const login_element = document.querySelector(""); // Fill the selector for the login element in ""
+
+/*
+const username=document.querySelector("#username");
+username.value="your_username";
+const password=document.querySelector("#password");
+password.value="your_password";
+*/
+
+const login_element=document.querySelector("#login");
 let login_text = login_element.innerText;
+const arr1=login_text.split("\n");
 
-let question = ""; // Use split and array operations on the login_text string to extract the question
+let question = arr1[3];
+const arr2=question.split(" ");
 
-let answer = ""; // Use if conditions to parse the question and calculate the answer. Make cases for all types of captcha asked
+let answer="";
 
-const captcha_input_element = document.querySelector(""); // Fill the selector for the captcha input element in ""
-captcha_input_element.value = answer;
+if(arr2[1]==="add"){
+    let a=parseInt(arr2[2]);
+    let b=parseInt(arr2[4]);
+    answer=`${a+b}`;
+}
+else if(arr2[1]==="subtract"){
+    let a=parseInt(arr2[2]);
+    let b=parseInt(arr2[4]);
+    answer=`${a-b}`;
+}
+else if(arr2[1]==="enter"){
+    if(arr2[2]==="first"){
+        answer=arr2[4];
+    }
+    else if(arr2[2]==="second"){
+        answer=arr2[6];
+    }
+}
+
+const captcha_input_element = document.querySelector("#valuepkg3"); 
+captcha_input_element.value=answer;
+
+//login_element.submit();
